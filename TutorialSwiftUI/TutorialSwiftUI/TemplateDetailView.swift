991622ff89c8a6b8b43820d34f6e688246a02c4f
youngwoo_ahn <codeway119@gmail.com>
2024-04-30 16:49:17 +0900

    TemplateDetailView Write

--- TutorialSwiftUI/TutorialSwiftUI/TemplateDetailView.swift
+++ TutorialSwiftUI/TutorialSwiftUI/TemplateDetailView.swift
@@ -1,15 +1,140 @@
 //
-//  TemplateDetailView.swift
-//  TutorialSwiftUI
+//  Detail001.swift
+//  Demo00_SwiftUIStudyApp
 //
-//  Created by user on 4/30/24.
+//  Created by user on 4/26/24.
 //
 
 import SwiftUI
 
+/// 사용할 모디파이어 기재
+private enum Modifire: String, CaseIterable {
+    case none
+    case bold
+    case frame
+    case background
+}
+
 struct TemplateDetailView: View {
+    
+    /// 각각의 Picker에서 선택될 enum 타입의 모디파이어 배열
+    @State private var selectedModifire: [Modifire] =
+    Array(repeating: .none, count: 10)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Picker - 위치별 모디파이어 선택
+            List {
+                Picker("First", selection: $selectedModifire[0]) {
+                    ForEach(Modifire.allCases, id: \.self) { modi in
+                        Text("\(modi.rawValue)")
+                            .tag(modi)
+                    }
+                }
+                Picker("Second", selection: $selectedModifire[1]) {
+                    ForEach(Modifire.allCases, id: \.self) { modi in
+                        Text("\(modi.rawValue)")
+                            .tag(modi)
+                    }
+                }
+                Picker("Third", selection: $selectedModifire[2]) {
+                    ForEach(Modifire.allCases, id: \.self) { modi in
+                        Text("\(modi.rawValue)")
+                            .tag(modi)
+                    }
+                }
+            }.frame(height: 200)
+            
+            // 코드 출력
+            VStack(alignment: .leading) {
+                Text("Text(\"TEST\")")
+                CodeBuilder(selectedModifire: $selectedModifire[0])
+                CodeBuilder(selectedModifire: $selectedModifire[1])
+                CodeBuilder(selectedModifire: $selectedModifire[2])
+                CodeBuilder(selectedModifire: $selectedModifire[3])
+            }
+            .frame(width: 350)
+            .font(.system(size: 20, design: .monospaced))
+            .background(Color.gray.opacity(0.1))
+            .cornerRadius(8)
+            
+            Spacer()
+            
+            // 코드 설명
+            VStack(alignment: .leading) {
+                Text("텍스트 뷰 : 텍스트를 출력해주는 뷰")
+                DescriptionBuilder(selectedModifire: $selectedModifire[0])
+                DescriptionBuilder(selectedModifire: $selectedModifire[1])
+                DescriptionBuilder(selectedModifire: $selectedModifire[2])
+                DescriptionBuilder(selectedModifire: $selectedModifire[3])
+            }
+            .frame(width: 350)
+            .font(.system(size: 20, design: .monospaced))
+            .background(Color.gray.opacity(0.1))
+            .cornerRadius(8)
+            
+            Spacer()
+            
+            // 코드 결과
+            Text("TEST")
+                .font(.largeTitle)
+                .modifier(ModifireBuilder(selectedModifire: $selectedModifire[0]))
+                .modifier(ModifireBuilder(selectedModifire: $selectedModifire[1]))
+                .modifier(ModifireBuilder(selectedModifire: $selectedModifire[2]))
+                .modifier(ModifireBuilder(selectedModifire: $selectedModifire[3]))
+            
+            Spacer()
+        }
+    }
+}
+
+/// 코드 출력
+private struct CodeBuilder: View {
+    @Binding var selectedModifire: Modifire
+    
+    var body: some View {
+        switch selectedModifire {
+        case .none:
+            return Text("")
+        case .bold:
+            return Text("\t.bold()")
+        case .frame:
+            return Text("\t.frame(width: 200, height: 100)")
+        case .background:
+            return Text("\t.background(Color.red)")
+        }
+    }
+}
+
+/// 코드 출력
+private struct DescriptionBuilder: View {
+    @Binding var selectedModifire: Modifire
+    
+    var body: some View {
+        switch selectedModifire {
+        case .none:
+            return Text("")
+        case .bold:
+            return Text("bold : 볼드는 어쩌구")
+        case .frame:
+            return Text("frame : 뷰 크기 설정")
+        case .background:
+            return Text("background : 배경 설정")
+        }
+    }
+}
+
+/// 모디파이어 설정
+private struct ModifireBuilder: ViewModifier {
+    @Binding var selectedModifire: Modifire
+    
+    func body(content: Content) -> some View {
+        switch selectedModifire {
+        case .none : content
+        case .bold : content.bold()
+        case .frame : content.frame(width: 200, height: 100)
+        case .background : content.background(Color.red)
+        }
     }
 }
 
