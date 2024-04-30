f9b5be378e10498718c3a52b213e209b05590e0b
youngwoo_ahn <codeway119@gmail.com>
2024-04-30 16:41:37 +0900

    TemplateDetailView Write

---
+++ TutorialSwiftUI/TutorialSwiftUI/TemplateDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TemplateDetailView.swift
+//  TutorialSwiftUI
+//
+//  Created by user on 4/30/24.
+//
+
+import SwiftUI
+
+struct TemplateDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TemplateDetailView()
+}
