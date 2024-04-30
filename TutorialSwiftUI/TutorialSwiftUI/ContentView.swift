ef72cadfc8c0d324d2d708464cf071cf5a26b53a
Ruel <c0407h@gmail.com>
2024-04-30 16:13:39 +0900

    Init] TutorialSwiftUI Project

---
+++ TutorialSwiftUI/TutorialSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TutorialSwiftUI
+//
+//  Created by Chung Wussup on 4/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TutorialSwiftUI/TutorialSwiftUI/TutorialSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialSwiftUIApp.swift
+//  TutorialSwiftUI
+//
+//  Created by Chung Wussup on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TutorialSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
