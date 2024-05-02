f1943968c24d1359ac879fdccf07c1de99741032
DopeKS <kyusang.dev@gmail.com>
2024-05-02 13:14:49 +0900

    add comments, annotate View Preview buttons

--- TutorialSwiftUI/TutorialSwiftUI/Navigation/NavigationContainerView.swift
+++ TutorialSwiftUI/TutorialSwiftUI/Navigation/NavigationContainerView.swift
@@ -40,7 +40,9 @@ struct NavigationContainerView: View {
                         .fontWeight(.bold)
                         .foregroundStyle(Color.black)
                     ) {
-                        Text("NavigationView 는 SwiftUI의 가장 중요한 구성 요소 중 하나입니다. 화면을 쉽게 Push 와 Pop할 수 있으며, 사용자에게 명확하고 계층적인 방식으로 정보를 제공할 수 있습니다")
+                        Text("""
+                             NavigationView는 SwiftUI의 초기 버전에서 사용되어 온 컴포넌트로, 사용자 인터페이스에서 계층적인 데이터 구조를 탐색할 수 있게 해주는 네비게이션 컨테이너입니다. 이는 사용자가 앱 내의 다양한 화면 사이를 이동할 수 있게 도와줍니다. iOS 16 이후로는 NavigationStack에 의해 대체되었지만, 여전히 기존 코드에서 사용되거나 특정 상황에서 유용하게 사용될 수 있습니다. \n\n NavigationView를 사용하면 네비게이션 바를 포함한 화면 구조를 쉽게 구현할 수 있으며, 네비게이션 링크(NavigationLink)를 통해 다른 뷰로의 전환을 관리할 수 있습니다.
+                             """)
                             .padding()
                             .padding(.top, -20)
                             .font(.system(size: 16))
@@ -49,29 +51,28 @@ struct NavigationContainerView: View {
                             .frame(width: 350)
                     }
                     
-                    Section(header: Text("View Preview")
-                        .textCase(nil)
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundStyle(Color.black)
-                    ) {
-                        HStack {
-                            Spacer()
-                            Button("Navigation View") {
-                                isPresented.toggle()
-                            }
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color.blue)
-                            .cornerRadius(10)
-                            Spacer()
-                        }
-                        .cornerRadius(10)
-                        .padding()
-                        .padding(.top, -20)
-                    }
-                    .listRowBackground(Color.clear)
-                    .frame(width: 350)
+//                    Section(header: Text("View Preview")
+//                        .textCase(nil)
+//                        .font(.title2)
+//                        .fontWeight(.bold)
+//                        .foregroundStyle(Color.black)
+//                    ) {
+//                        HStack {
+//                            Spacer()
+//                            Button("Navigation View") {
+//                                isPresented.toggle()
+//                            }
+//                            .foregroundColor(.white)
+//                            .padding()
+//                            .background(Color.blue)
+//                            .cornerRadius(10)
+//                            Spacer()
+//                        }
+//                        .cornerRadius(10)
+//                        .padding()
+//                        .padding(.top, -20)
+//                        .listRowBackground(Color.clear)
+//                    }
                     
                     Section(header: Text("Base Code")
                         .textCase(nil)
---
+++ TutorialSwiftUI/TutorialSwiftUI/Navigation/NavigationStackContainer.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationStackContainer.swift
+//  TutorialSwiftUI
+//
+//  Created by 황규상 on 5/2/24.
+//
+
+import SwiftUI
+
+struct NavigationStackContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationStackContainer()
+}
