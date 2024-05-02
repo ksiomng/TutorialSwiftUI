b75f9f79c2f8cdfeabefa31e639e703634fc3a44
youngwoo_ahn <codeway119@gmail.com>
2024-05-02 23:30:45 +0900

    ScrollView - 일부 수정

---
+++ TutorialSwiftUI/TutorialSwiftUI/Collections/LazyHGridView.swift
@@ -0,0 +1,18 @@
+//
+//  LazyHGrid.swift
+//  TutorialSwiftUI
+//
+//  Created by user on 5/2/24.
+//
+
+import SwiftUI
+
+struct LazyHGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyHGrid()
+}
---
+++ TutorialSwiftUI/TutorialSwiftUI/Collections/LazyVGridView.swift
@@ -0,0 +1,18 @@
+//
+//  LazyVGridView.swift
+//  TutorialSwiftUI
+//
+//  Created by user on 5/2/24.
+//
+
+import SwiftUI
+
+struct LazyVGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyVGridView()
+}
