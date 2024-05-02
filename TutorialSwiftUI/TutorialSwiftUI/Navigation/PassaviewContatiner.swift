3fc5be9f9d543fb4459cfdecfe43c0f4ea179246
DopeKS <kyusang.dev@gmail.com>
2024-05-02 15:50:58 +0900

    Add Pass a view

---
+++ TutorialSwiftUI/TutorialSwiftUI/Navigation/PassaviewContatiner.swift
@@ -0,0 +1,18 @@
+//
+//  PassaviewContatiner.swift
+//  TutorialSwiftUI
+//
+//  Created by 황규상 on 5/2/24.
+//
+
+import SwiftUI
+
+struct PassaviewContatiner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PassaviewContatiner()
+}
