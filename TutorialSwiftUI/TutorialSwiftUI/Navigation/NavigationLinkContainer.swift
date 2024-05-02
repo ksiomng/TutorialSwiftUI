47b8819f536e3cc722a136a91f52aad5c2c8c996
DopeKS <kyusang.dev@gmail.com>
2024-05-02 14:51:10 +0900

    Add NavigationLink

---
+++ TutorialSwiftUI/TutorialSwiftUI/Navigation/NavigationLinkContainer.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationLInkContainer.swift
+//  TutorialSwiftUI
+//
+//  Created by 황규상 on 5/2/24.
+//
+
+import SwiftUI
+
+struct NavigationLinkContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationLinkContainer()
+}
